d0832c6cfaa89bc27ccb3ccb195aa0f42ea8e8fd
anaypant <35899820+anaypant@users.noreply.github.com>
2021-09-03 14:49:43 -0700

    c

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,25 +1,34 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class MyMain {
 
     // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        if (a > b){
-            if (a > c){
-                return c;
+        if (a > b) {
+            if (a > c) {
+                if (c > b) {
+                    return c;
+                }
             }
             return a;
         }
-        if (b > c){
+        if (b > c) {
+            if (a > c) {
+                return a;
+            }
             return c;
+
         }
         return b;
 
     }
-
     // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return Math.max(Math.abs(a), Math.abs(b)); // REPLACE WITH YOUR CODE
+        if (Math.max(Math.abs(a), Math.abs(b)) == a){
+            return a;
+        }
+        return b;
     }
 
     // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
@@ -38,7 +47,7 @@ public class MyMain {
         // YOUR CODE HERE
         System.out.println("median, magnitude, or pythagoras");
         String choice = scan.next();
-        if (choice == "median"){
+        if (Objects.equals(choice, "median")){
             System.out.println("Select a: ");
             int a = Integer.parseInt(scan.next());
             System.out.println("Select b: ");
@@ -47,13 +56,14 @@ public class MyMain {
             int c = Integer.parseInt(scan.next());
             System.out.println(median(a, b, c));
         }
-        else if (choice == "magnitude"){
+        else if (Objects.equals(choice, "magnitude")){
             int a = Integer.parseInt(scan.next());
             System.out.println("Select b: ");
             int b = Integer.parseInt(scan.next());
             System.out.println(magnitude(a, b));
         }
-        else if (choice == "pythagoras"){
+        else if (Objects.equals(choice, "pythagoras")){
+            System.out.println("Select a: ");
             int a = Integer.parseInt(scan.next());
             System.out.println("Select b: ");
             int b = Integer.parseInt(scan.next());
